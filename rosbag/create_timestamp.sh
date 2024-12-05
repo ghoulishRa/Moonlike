33603116882873fab02993c532febfb7b26b37cc
Israel <a01027029@tec.mx>
2024-12-05 00:47:22 -0600

    uploading readme, renaming workspaces, and some bash files

---
+++ rosbag/create_timestamp.sh
@@ -0,0 +1,3 @@
+cd rosbag/
+
+mkdir -p timestamp/ camera_01/depth camera_01/image camera_01/ir camera_01/pcl \camera_02/depth camera_02/image camera_02/ir camera_02/pcl thermal lidar pose aligned_pcl
